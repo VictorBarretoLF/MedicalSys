305a6d9d9e38f9692c411f5ed6fdc3bff4ebfca2
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-24 14:34:42 -0300

    intalled axios

--- backend/users/models.py
+++ backend/users/models.py
@@ -37,9 +37,8 @@ class CustomAccountManager(BaseUserManager):
 class NewUser(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(_('email address'), unique=True)
-    user_name = models.CharField(max_length=150, unique=True)
-    first_name = models.CharField(max_length=150, blank=True)
-    start_date = models.DateTimeField(default=timezone.now)
+    nome = models.CharField(max_length=250)
+    data_criacao = models.DateTimeField(default=timezone.now)
     about = models.TextField(_(
         'about'), max_length=500, blank=True)
     is_staff = models.BooleanField(default=False)
