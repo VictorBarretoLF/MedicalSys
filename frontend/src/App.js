59f107322e36ec09b208b935b03f01b06f7b29d9
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-24 09:18:34 -0300

    finished structuring the routes and pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
+import { Fragment } from "react";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Authentication from "./pages/Authentication";
+
+const PrivateLayout = () => {
+  return (
+    <Fragment>
+      <Navbar />
+      <Outlet />
+    </Fragment>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Authentication />,
+  },
+  {
+    path: "app",
+    element: <PrivateLayout />,
   },
 ]);
 
--- frontend/src/pages/PatientScheduling.js
+++ frontend/src/pages/PatientScheduling.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PatientScheduling = () => {
     return (
         <div>
-            
+            <h1>Agendamento de Pacientes</h1>
         </div>
     );
 };
