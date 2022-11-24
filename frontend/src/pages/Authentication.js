9852eec2385a185c09daa32b6f4f451f179c545b
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-24 09:18:12 -0300

    adding boostrap tab fot the authentication

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from "react";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
 const Authentication = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [key, setKey] = useState("home");
+  return (
+    <div>
+      <Tabs
+        id="controlled-tab-example"
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
+        className="mb-3"
+      >
+        <Tab eventKey="home" title="Home">
+          <h1>Hello</h1>
+        </Tab>
+        <Tab eventKey="profile" title="Profile">
+          <h1>Hi</h1>
+        </Tab>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Authentication;
