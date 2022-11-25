ab8d9912f29128b94066eb310b5164bab82b3c70
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-24 21:58:48 -0300

    adding the configModal to the PatientCards

--- frontend/src/components/PatientCard.js
+++ frontend/src/components/PatientCard.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
+import PatientsConfigModal from "../components/PatientsConfigModal";
 
 const PatientCard = ({ data }) => {
+  const [modalShow, setModalShow] = useState(false);
   return (
     <Card className="mb-4" style={{ width: "16.3rem" }}>
-      <Card.Header>
+      <Card.Header className="d-flex justify-content-between align-items-center">
         {data.name.length > 20 ? data.name.slice(0, 20) + "..." : data.name}
+        <Button variant="success" onClick={() => setModalShow(true)}>
+          Info
+        </Button>
+
+        <PatientsConfigModal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+        />
       </Card.Header>
       <ListGroup variant="flush">
         <ListGroup.Item>Telefone: {data.telephone}</ListGroup.Item>
