7e6e40e7a384354eb6ebb3a383ba719e9d4df50a
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-24 21:48:15 -0300

    applaying boostrap grid to show the list of patients

--- frontend/src/pages/ManagePatients.js
+++ frontend/src/pages/ManagePatients.js
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
 import PatientsConfigModal from "../components/PatientsConfigModal";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import PatientCard from "../components/PatientCard";
 
 const ManagePatients = () => {
   const [modalShow, setModalShow] = useState(false);
+  const [patients, setPatients] = useState([]);
+  console.log(patients);
+  useEffect(() => {
+    const getPatients = async () => {
+      const res = await axios.get("http://localhost:8000/api/");
+      setPatients(res.data);
+    };
+    getPatients();
+  }, []);
+
   return (
     <div className="h-100">
       <header className="py-2 px-4 d-flex justify-content-sm-end">
@@ -17,6 +32,19 @@ const ManagePatients = () => {
           onHide={() => setModalShow(false)}
         />
       </header>
+      <main className="px-4 mt-2">
+        <Container>
+          <Row>
+            {patients.map((patient) => {
+              return (
+                <Col key={patient.id}>
+                  <PatientCard data={patient} />
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      </main>
     </div>
   );
 };
