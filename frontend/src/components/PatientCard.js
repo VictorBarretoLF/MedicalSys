10945ccd0dd3b44a025d3d0401ef64cc1253f1f2
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-25 15:28:05 -0300

    added the modal for each patient card and its props

--- frontend/src/components/PatientCard.js
+++ frontend/src/components/PatientCard.js
@@ -3,24 +3,40 @@ import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import Button from "react-bootstrap/Button";
 import PatientsConfigModal from "../components/PatientsConfigModal";
+import DeletePatientModal from "./DeletePatientModal";
 
-const PatientCard = ({ data }) => {
+const PatientCard = ({ data, patientIndex }) => {
   const [infoModal, setInfoModal] = useState(false);
+  const [deleteModal, setDeleteModal] = useState(false);
+
   return (
     <Card className="mb-4">
       <Card.Header className="d-flex justify-content-between align-items-center">
         {data.name.length > 40 ? data.name.slice(0, 40) + "..." : data.name}
-        <Button variant="info" onClick={() => setInfoModal(true)}>
-          Info
-        </Button>
+        <div className="d-flex gap-3">
+          <Button variant="info" onClick={() => setInfoModal(true)}>
+            Info
+          </Button>
 
-        <PatientsConfigModal
-          show={infoModal}
-          onHide={() => setInfoModal(false)}
-          propData={data}
-          edit={true}
-          patientId={data.id}
-        />
+          <Button variant="danger" onClick={() => setDeleteModal(true)}>
+            Delete
+          </Button>
+
+          <PatientsConfigModal
+            show={infoModal}
+            onHide={() => setInfoModal(false)}
+            propData={data}
+            edit={true}
+            patientId={data.id}
+          />
+
+          <DeletePatientModal
+            show={deleteModal}
+            onHide={() => setDeleteModal(false)}
+            patientData={data}
+            patientIndex={patientIndex}
+          />
+        </div>
       </Card.Header>
       <ListGroup variant="flush">
         <ListGroup.Item>Telefone: {data.telephone}</ListGroup.Item>
