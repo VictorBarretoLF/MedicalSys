b59dbf8782786c7dbaaa89ba75edd66c1fd4ea87
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-25 13:28:14 -0300

    applying the PatientProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { PatientProvider } from "./context/PatientContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <PatientProvider>
+      <App />
+    </PatientProvider>
   </React.StrictMode>
 );
 
