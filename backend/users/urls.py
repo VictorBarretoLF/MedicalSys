cb50f0ada686c99b2b274adc3bbfdd754076bede
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-26 17:57:20 -0300

    added blacklist token url

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import CustomUserCreate, UserList #, BlacklistTokenUpdateView
+from .views import CustomUserCreate, UserList , BlacklistTokenUpdateView
 
 app_name = 'users'
 
 urlpatterns = [
     path('create/', CustomUserCreate.as_view(), name="create_user"),
     path('all/', UserList.as_view(), name="get_all_users"),
-    # path('logout/blacklist/', BlacklistTokenUpdateView.as_view(),
-    #      name='blacklist')
+    path('logout/blacklist/', BlacklistTokenUpdateView.as_view(), name='blacklist')
 ]
