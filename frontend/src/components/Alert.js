7ad5f02148ab62e4beb82f207a3d5d01acea7310
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-26 13:53:47 -0300

    created a component to show an alert for the user

---
+++ frontend/src/components/Alert.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import Alert from "react-bootstrap/Alert";
+
+const CustomAlert = ({ type, msg, removeAlert }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 5000);
+    return () => clearTimeout(timeout);
+  }, []);
+  return <Alert variant={type}>{msg}</Alert>;
+};
+
+export default CustomAlert;
