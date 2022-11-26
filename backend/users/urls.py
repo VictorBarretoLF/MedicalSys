661f14a4db5459aa28a8fac1ab5db318d0872752
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-26 16:16:56 -0300

    created a get all users method

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import CustomUserCreate #, BlacklistTokenUpdateView
+from .views import CustomUserCreate, UserList #, BlacklistTokenUpdateView
 
 app_name = 'users'
 
 urlpatterns = [
     path('create/', CustomUserCreate.as_view(), name="create_user"),
+    path('all/', UserList.as_view(), name="get_all_users"),
     # path('logout/blacklist/', BlacklistTokenUpdateView.as_view(),
     #      name='blacklist')
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -5,6 +5,8 @@ from rest_framework.views import APIView
 from .serializers import CustomUserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny
+from rest_framework import generics
+from .models import NewUser
 
 
 class CustomUserCreate(APIView):
@@ -18,3 +20,7 @@ class CustomUserCreate(APIView):
                 json = serializer.data
                 return Response(json, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserList(generics.ListAPIView):
+    queryset = NewUser.objects.all()
+    serializer_class = CustomUserSerializer
