a448aa623232f16cc8acf8e4ad9ee14b7ea42da0
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-27 15:25:56 -0300

    created the views for the scheduling

---
+++ backend/scheduling/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics
+from .models import Scheduling
+from .serializers import SchedulingSerializer
+from rest_framework.permissions import IsAuthenticated
+
+
+class SchedulingList(generics.ListCreateAPIView):
+    # permission_classes = [IsAuthenticated]
+    queryset = Scheduling.objects.all()
+    serializer_class = SchedulingSerializer
+
+
+class SchedulingDetail(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = [IsAuthenticated]
+    queryset = Scheduling.objects.all()
+    serializer_class = SchedulingSerializer
