de04f4255ac0f9d660bdf12d93cd2f28c902154a
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-27 15:26:30 -0300

    created the serializers for the scheduling

---
+++ backend/scheduling/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Scheduling
+
+class SchedulingSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ('id', 'description', 'status', 'date', 'doctor', 'patient')
+        model = Scheduling
+        # depth = 1
