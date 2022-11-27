1a384a23c886f4b6f7df2d817edbbf68c75e31fe
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-27 12:22:25 -0300

    created a function to get the current user data

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,25 +1,35 @@
 import { createContext, useEffect, useState } from "react";
 import produce from "immer";
 import axiosInstance from "../utils/axios";
+import jwt_decode from "jwt-decode";
 
 export const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState({
-    access_token: localStorage.getItem("access_token")
+  const [auth, setAuth] = useState(
+    localStorage.getItem("access_token")
       ? localStorage.getItem("access_token")
-      : null,
-    refresh_token: localStorage.getItem("refresh_token")
-      ? localStorage.getItem("refresh_token")
-      : null,
-  });
+      : null
+  );
+
+  const [userData, setUserData] = useState({});
 
   useEffect(() => {
-    console.log(auth);
+    const getCurrentUserData = async () => {
+      const user_id = jwt_decode(auth).user_id;
+
+      try {
+        const res = await axiosInstance.get(`user/${user_id}/`);
+        setUserData(res.data);
+      } catch (error) {
+        alert("Um erro aconteceu!");
+      }
+    };
+    if (auth) getCurrentUserData();
   }, [auth]);
 
   return (
-    <AuthContext.Provider value={{ auth, setAuth }}>
+    <AuthContext.Provider value={{ auth, setAuth, userData }}>
       {children}
     </AuthContext.Provider>
   );
