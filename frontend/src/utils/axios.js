1a0f0a6f9543b7da7a2bcf34d196fc5d47da823e
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-28 09:53:18 -0300

    commented two unutilized imports

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import jwt_decode from "jwt-decode";
-import dayjs from "dayjs";
+// import jwt_decode from "jwt-decode";
+// import dayjs from "dayjs";
 
 const baseURL = "http://127.0.0.1:8000/api/";
 
