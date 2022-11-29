60f3888bff4eddf13c40a3c4c6a8cedad390cdc3
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-29 15:46:04 -0300

    setting up permissions for the users

--- backend/scheduling/views.py
+++ backend/scheduling/views.py
@@ -5,11 +5,13 @@ from rest_framework.permissions import IsAuthenticated
 
 
 class SchedulingList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Scheduling.objects.all()
     serializer_class = SchedulingSerializer
 
 
 class SchedulingDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Scheduling.objects.all()
     serializer_class = SchedulingSerializer
 
