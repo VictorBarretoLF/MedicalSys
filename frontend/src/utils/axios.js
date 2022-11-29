52b1b1d1b0e4851d79a7d9801c1a244fbd828624
Víctor Barreto <victorbarretolins@gmail.com>
2022-11-29 14:28:26 -0300

    working with Bearer token in axiosInstace

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,14 +2,14 @@ import axios from "axios";
 // import jwt_decode from "jwt-decode";
 // import dayjs from "dayjs";
 
-const baseURL = "http://127.0.0.1:8000/api/";
+const baseURL = "http://127.0.0.1:8000/";
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
   timeout: 10000,
   headers: {
     Authorization: localStorage.getItem("access_token")
-      ? "JWT " + localStorage.getItem("access_token")
+      ? "Bearer " + localStorage.getItem("access_token")
       : null,
     "Content-Type": "application/json",
     accept: "application/json",
